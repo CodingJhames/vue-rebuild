dc3b81819953b5a74e8534f5babda595ac342804
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-03-25 15:06:29 -0500

    declarative representation

--- bases-vue/app.js
+++ bases-vue/app.js
@@ -1,14 +1,11 @@
 
 const app = Vue.createApp({
 
-    template: `
-    
-        <h1> Hello Parchis </h1>
-        <p> From app.js </p>
-    
-    
-    `
+    // template: `
 
+    //     <h1> Hello Parchis </h1>
+    //     <p> From app.js </p>
+    // `
 })
 
 app.mount('#myApp');
